a228ed44c5b1a44c8d9491976dbb2e9d3d8a67b3
Roman Prodan <romeo_82@yahoo.com>
2022-07-15 13:52:46 +0300

    added hometasks 1 and 2

--- view/hometask_1_Roman_Prodan.py
+++ view/hometask_1_Roman_Prodan.py
@@ -1,42 +1,45 @@
-import requests
 import csv
+import requests
 
-RP_result = "G:\\PythonWorkshop\\sum_RP_result.csv"
+RP_RESULT = "D:\\Work\\work_folder\\PythonWorkshop\\RP_result.csv"
 
-locations = ('eastasia', 'southeastasia', 'centralus', 'eastus', 'eastus2', 'westus', 'northcentralus', 'southcentralus', 'northeurope', 'westeurope', 'japanwest', 'japaneast', 'brazilsouth', 'australiaeast', 'australiasoutheast',  'southindia', 'centralindia', 'westindia', 'canadacentral', 'canadaeast', 'uksouth', 'ukwest', 'westcentralus',  'westus2', 'koreacentral', 'koreasouth', 'francecentral', 'francesouth', 'australiacentral', 'australiacentral2', 'uaecentral', 'uaenorth', 'southafricanorth', 'southafricawest', 'switzerlandnorth', 'switzerlandwest', 'germanynorth', 'germanywestcentral', 'norwaywest', 'norwayeast', 'brazilsoutheast', 'westus3')
-option = "productName eq 'General Block Blob v2' and skuName eq 'Cool LRS'"
+LOCATIONS = ('eastasia', 'southeastasia', 'centralus', 'eastus', 'eastus2', 'westus', 'northcentralus',
+             'southcentralus', 'northeurope', 'westeurope', 'japanwest', 'japaneast', 'brazilsouth', 'australiaeast',
+             'australiasoutheast',  'southindia', 'centralindia', 'westindia', 'canadacentral', 'canadaeast',
+             'uksouth', 'ukwest', 'westcentralus',  'westus2', 'koreacentral', 'koreasouth', 'francecentral',
+             'francesouth', 'australiacentral', 'australiacentral2', 'uaecentral', 'uaenorth', 'southafricanorth',
+             'southafricawest', 'switzerlandnorth', 'switzerlandwest', 'germanynorth', 'germanywestcentral',
+             'norwaywest', 'norwayeast', 'brazilsoutheast', 'westus3')
+
+OPTION = "productName eq 'General Block Blob v2' and skuName eq 'Cool LRS'"
+
 result_prices = {}
 
-for location in locations:
-    api_endpoint = f"https://prices.azure.com/api/retail/prices?$filter=armRegionName eq '{location}' and {option}"
+for location in LOCATIONS:
+    api_endpoint = f"https://prices.azure.com/api/retail/prices?$filter=armRegionName eq '{location}' and {OPTION}"
     r = requests.get(api_endpoint)
     data = r.json()
     data_items = data['Items']
-    sumRetail = 0
+    SUM_RETAIL = 0
     for offer in data_items:
-        sumRetail += offer['retailPrice']
-    result_prices[location] = sumRetail
+        SUM_RETAIL += offer['retailPrice']
+    result_prices[location] = SUM_RETAIL
     location_results = result_prices.keys()
     retailPrices_results = result_prices.values()
 
-    with open(RP_result, "w", newline='') as csv_file:
+    with open(RP_RESULT, "w", newline='') as csv_file:
         writer = csv.writer(csv_file)
         writer.writerows([location_results, retailPrices_results])
 
-minretailPrices = min(result_prices.values())
-maxretailPrices = max(result_prices.values())
-sumretailPrices = sum(result_prices.values())
-lengthretailPrices = len(result_prices.values())
-avgretailPrices = sumretailPrices/lengthretailPrices
+minRetailPrices = min(result_prices.values())
+maxRetailPrices = max(result_prices.values())
+sumRetailPrices = sum(result_prices.values())
+lengthRetailPrices = len(result_prices.values())
+avgRetailPrices = sumRetailPrices/lengthRetailPrices
 
-additional_result = {'minPrice': minretailPrices, 'maxPrices': maxretailPrices, 'avgPrice': avgretailPrices}
+additional_result = {'minPrice': minRetailPrices, 'maxPrices': maxRetailPrices, 'avgPrice': avgRetailPrices}
 additional_result_keys = additional_result.keys()
 additional_result_values = additional_result.values()
-with open(RP_result, "a", newline='') as csv_file:
+with open(RP_RESULT, "a", newline='') as csv_file:
     writer = csv.writer(csv_file)
     writer.writerows([additional_result_keys, additional_result_values])
-
-
-
-
-
--- view/hometask_2_Roman_Prodan.py
+++ view/hometask_2_Roman_Prodan.py
@@ -1,18 +1,18 @@
 import csv
 
-syslog_file = "D:\\Work\\work_folder\\PythonWorkshop\\syslog"
-my_syslog_file = "D:\\Work\\work_folder\\PythonWorkshop\\my_syslog_file.csv"
+SYS_LOG_FILE = "D:\\Work\\work_folder\\PythonWorkshop\\syslog"
+MY_SYSLOG_FILE = "D:\\Work\\work_folder\\PythonWorkshop\\my_syslog_file.csv"
 
-with open(syslog_file, "r", encoding="utf8") as syslog_file, open(my_syslog_file, "w", newline='') as csv_file:
+with open(SYS_LOG_FILE, "r", encoding="utf8") as syslog_file, open(MY_SYSLOG_FILE, "w", newline='') as csv_file:
     lines = syslog_file.readlines()
-    my_type_source = "WARNING kernel"
+    MY_TYPE_SOURCE: str = "WARNING kernel"
     for string in lines:
-        if my_type_source in string:
+        if MY_TYPE_SOURCE in string:
             date_colum = string.split(',')[0]
             message_colum = string.split(':')[3:]
             writer = csv.writer(csv_file)
             writer.writerow([date_colum, message_colum])
 
-            colum1 = "Colum_1: {date}".format(date=date_colum)
-            colum2 = "Colum_2: {message}".format(message=message_colum)
-            print(colum1 + ", " + colum2)
+            COLUM1 = "Colum_1: {date}".format(date=date_colum)
+            COLUM2 = "Colum_2: {message}".format(message=message_colum)
+            print(COLUM1 + ", " + COLUM2)
