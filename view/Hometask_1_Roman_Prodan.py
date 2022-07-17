59cb36a572a7e17e172a1115fe1ed1dab743bb21
Roman Prodan <romeo_82@yahoo.com>
2022-07-17 10:17:24 +0300

    added hometasks 1 and 2

--- view/Hometask_1_Roman_Prodan.py
+++ view/Hometask_1_Roman_Prodan.py
@@ -21,20 +21,25 @@ OPTION = "productName eq 'General Block Blob v2' and skuName eq 'Cool LRS'"
 result_prices = {}
 
 for location in LOCATIONS:
-    api_endpoint = f"https://prices.azure.com/api/retail/prices?$filter=armRegionName eq '{location}' and {OPTION}"
+    api_endpoint = f"https://prices.azure.com/api/retail/prices?$filter=armRegionName " \
+                   f"eq '{location}' and {OPTION}"
     r = requests.get(api_endpoint)
     data = r.json()
     data_items = data['Items']
     SUM_RETAIL = 0
     for offer in data_items:
         SUM_RETAIL += offer['retailPrice']
-    result_prices[location] = SUM_RETAIL
-    location_results = result_prices.keys()
-    retailPrices_results = result_prices.values()
+        result_prices[location] = SUM_RETAIL
+        location_results = result_prices.keys()
+        retailPrices_results = result_prices.values()
 
-    with open(RP_RESULT, "w", newline='') as csv_file:
-        writer = csv.writer(csv_file)
-        writer.writerows([location_results, retailPrices_results])
+        with open(RP_RESULT, "w", encoding="utf8", newline='') as csv_file:
+            columns = [location_results, retailPrices_results]
+            writer = csv.DictWriter(csv_file, fieldnames=columns)
+            #writer = csv.writer(csv_file)
+            #writer.writeheader()
+            writer.writerows(result_prices)
+            #writer.writerows([location_results, retailPrices_results])
 
 minRetailPrices = min(result_prices.values())
 maxRetailPrices = max(result_prices.values())
@@ -42,13 +47,11 @@ sumRetailPrices = sum(result_prices.values())
 lengthRetailPrices = len(result_prices.values())
 avgRetailPrices = sumRetailPrices/lengthRetailPrices
 
-additional_result = {
-    'minPrice': minRetailPrices,
-    'maxPrices': maxRetailPrices,
-    'avgPrice': avgRetailPrices
-}
-additional_result_keys = additional_result.keys()
-additional_result_values = additional_result.values()
-with open(RP_RESULT, "a", newline='') as csv_file:
+additional_result = [
+    ['minPrice', minRetailPrices],
+    ['maxPrices', maxRetailPrices],
+    ['avgPrice', avgRetailPrices]
+]
+with open(RP_RESULT, "a", encoding="utf8", newline='') as csv_file:
     writer = csv.writer(csv_file)
-    writer.writerows([additional_result_keys, additional_result_values])
+    writer.writerows(additional_result)
